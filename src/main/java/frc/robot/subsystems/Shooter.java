c1f09b054f2346b30a0ddbaa7a12faee265c3db4
Sid T <thomas.siddharth@gmail.com>
2024-03-01 13:28:42 -0800

    Add comments for shooter code

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,17 +1,18 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.PhysConsts;
 import frc.robot.Constants.ShooterConsts;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 
-/** Controls indexer wheels. */
+// TODO(shooter): TLDR theres a lot of stuff here that is clearly unneded or implemented wrong, make sure you understand eveyrthing in here and can explain why its there and when its used.
+
+// TODO(shooter): this should extend SafeSubsystem. see ExampleSubsystem to see how to implement relevant methods
+/** Controls indexer wheels. */ // TODO(shooter): Thought this controlled shooter wheels and indexer wheels?
 public class Shooter extends SubsystemBase {
   private static Shooter m_instance;
 
@@ -23,20 +24,17 @@ public class Shooter extends SubsystemBase {
     return m_instance;
   }
 
-  TalonFX holdingMotor = new TalonFX(0);
-
   /** Used to apply tension if a game piece is being held in the intake. */
-  private static boolean m_holding = false;
+  private static boolean m_holding = false; // TODO(shooter): this is never set to true. Is it needed? delete otherwise.
 
-  /** Used to invert wheels */
+  /** Used to invert wheels */ // TODO(shooter): invert wheels when? what for? add docs
   private static boolean m_inverted = false;
-    private final static CANSparkMax
-      m_motor = new CANSparkMax(ShooterConsts.kShooterID, CANSparkLowLevel.MotorType.kBrushless),
-      neoMotorWithSparksMax = new CANSparkMax(ShooterConsts.kShooterID, CANSparkLowLevel.MotorType.kBrushless);
 
-    static { neoMotorWithSparksMax.follow(m_motor); }
+  private final static CANSparkMax m_motor = new CANSparkMax(ShooterConsts.kShooterID, MotorType.kBrushless);
 
-  private static final RelativeEncoder m_encoder = m_motor.getEncoder();
+  TalonFX holdingMotor = new TalonFX(0); // TODO(shooter): this variable should be final and follow the naming system used elsewhere (m_holdingMotor or similar). Also, put the ID in ShooterConsts
+
+  private static final RelativeEncoder m_encoder = m_motor.getEncoder(); // TODO(shooter): what is this encoder used for? delete if unnecessary. Also, you need to set conversion factors for the pulleys/gears before you use this data to have accurate units (ratios go into PhysConsts)
 
   private Shooter() {
     m_encoder.setMeasurementPeriod(20);
@@ -49,30 +47,34 @@ public class Shooter extends SubsystemBase {
     ));
   }
 
+  // TODO(shooter): if this is a command, it should say so. Add docs. Also, why is this a command? Finally, why is inverted set to false during the duration of the command? And why doesn't it require any subsystem?
   public Command invert() {
     return startEnd(() -> m_inverted = false, () -> m_inverted = true);
   }
 
-  public double getSpeed() {return m_motor.get();}
+  public double getSpeed() {return m_motor.get();} // TODO(shooter): Document units. Do we need this method for anything? If we do, it should be returning RPM or RPS measured by encoder
 
+  // TODO(shooter): theres another motor to stop too...
   public static void stop() {
     m_motor.stopMotor();
   }
 
   /** @return a command to intake a game piece */
+  // TODO(shooter): then why is it called getShootCommand?
   public Command getShootCommand() {
     return startEnd(
       () -> {
-        m_motor.setVoltage(m_inverted ? ShooterConsts.kSourceIntakeVolts : -ShooterConsts.kSourceIntakeVolts * 4);
+        m_motor.setVoltage(m_inverted ? ShooterConsts.kSourceIntakeVolts : -ShooterConsts.kSourceIntakeVolts * 4); // TODO(shooter): why is this inverted sometimes?
       },
       Shooter::stop
     );
   }
 
+  // TODO(shooter): add documentation, have it stop automatically after fed
   public Command getFeedCommand() {
     return startEnd(
       () -> {
-        holdingMotor.setVoltage(12);
+        holdingMotor.setVoltage(12); // TODO(shooter): trust me, put this number in ShooterConsts
       },
       Shooter::stop);
   }
