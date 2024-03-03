2267493ec32cd054ca161452f38e2b217d230748
Sid T <thomas.siddharth@gmail.com>
2024-03-02 17:40:22 -0800

    Formatting fix

--- src/main/java/frc/robot/commands/ShooterShoot.java
+++ src/main/java/frc/robot/commands/ShooterShoot.java
@@ -8,32 +8,27 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.subsystems.Shooter;
 
 public class ShooterShoot extends Command {
-    
-    private static final Shooter sShooter = Shooter.getInstance();
-    private static final Set<Subsystem> m_requirements = Set.of(sShooter);
+  private static final Shooter sShooter = Shooter.getInstance();
+  private static final Set<Subsystem> m_requirements = Set.of(sShooter);
 
-    @Override
-    public void initialize(){
-        
-    }
+  @Override
+  public void initialize(){}
 
-    @Override
-    public void execute() {
-       Shooter.m_ShooterBottom.setVoltage(ShooterConsts.kShootVolts);
-    }
+  @Override
+  public void execute() {
+    Shooter.m_ShooterBottom.setVoltage(ShooterConsts.kShootVolts);
+  }
 
-    @Override
-    public boolean isFinished() {
-        return false;
-    }
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
 
-    @Override
-    public void end(boolean interrupted) {
+  @Override
+  public void end(boolean interrupted) {}
 
-    }
-
-    @Override
-    public Set<Subsystem> getRequirements() {
-        return m_requirements;
-    }
+  @Override
+  public Set<Subsystem> getRequirements() {
+    return m_requirements;
+  }
 }
