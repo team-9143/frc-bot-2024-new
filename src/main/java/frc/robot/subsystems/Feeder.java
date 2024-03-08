201eec337c97bcb67e98c7111475292d3d69b50d
Sid T <thomas.siddharth@gmail.com>
2024-03-07 17:22:41 -0800

    Fix feeder motor configs

--- src/main/java/frc/robot/subsystems/Feeder.java
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.FeederConsts;
+import frc.robot.logger.Logger;
+import java.util.function.Supplier;
 
 /** Controls feeder wheels */
 public class Feeder extends SafeSubsystem {
@@ -16,31 +22,44 @@ public class Feeder extends SafeSubsystem {
     return m_instance;
   }
 
-  private static final TalonFX m_feedMotor = new TalonFX(FeederConsts.kFeedMotorID);
+  private static final TalonFXConfiguration configs =
+      new TalonFXConfiguration()
+          .withCurrentLimits(
+              new CurrentLimitsConfigs()
+                  .withSupplyCurrentLimit(40)
+                  .withSupplyCurrentLimitEnable(true))
+          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
+
+  private static final TalonFX m_motor = new TalonFX(FeederConsts.kFeedMotorID);
+  private static final Supplier<Double> m_voltageSupplier;
 
   static {
-    // TODO(shooter): Extra - use the talon configurator to set current limits and neutral mode
-    // here.
-    m_feedMotor.optimizeBusUtilization();
+    m_motor.getConfigurator().apply(configs);
+    var voltageSignal = m_motor.getMotorVoltage();
+    voltageSignal.setUpdateFrequency(50);
+    m_voltageSupplier = voltageSignal.asSupplier();
+    m_motor.optimizeBusUtilization();
   }
 
   private Feeder() {}
 
   /** Returns a command to feed a note from the intake to the shooter */
   public Command getFeedCommand() {
-    return startEnd(() -> m_feedMotor.setVoltage(FeederConsts.kFeedVolts), this::stop);
+    return startEnd(() -> m_motor.setVoltage(FeederConsts.kFeedVolts), this::stop);
   }
 
   /** Returns a command to deeply intake a note from the source */
   public Command getDeepIntakeCommand() {
-    return startEnd(() -> m_feedMotor.setVoltage(FeederConsts.kDeepIntakeVolts), this::stop);
+    return startEnd(() -> m_motor.setVoltage(FeederConsts.kDeepIntakeVolts), this::stop);
   }
 
   @Override
   public void stop() {
-    m_feedMotor.stopMotor();
+    m_motor.stopMotor();
   }
 
   @Override
-  public void log() {}
+  public void log() {
+    Logger.recordOutput(getDirectory() + "voltage", m_voltageSupplier.get());
+  }
 }
