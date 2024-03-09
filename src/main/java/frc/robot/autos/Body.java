68ca16b7c1f4b51f464accdcda4426c1ff4024b2
Sid T <thomas.siddharth@gmail.com>
2024-03-08 17:54:18 -0800

    Fix wing escape auto

--- src/main/java/frc/robot/autos/Body.java
+++ src/main/java/frc/robot/autos/Body.java
@@ -1,10 +1,9 @@
 package frc.robot.autos;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.Drivetrain;
 
 public enum Body implements MutableChooser.Named {
   Escape("Escape"),
@@ -24,8 +23,7 @@ public enum Body implements MutableChooser.Named {
     switch (this) {
       case Escape:
         if (startPose == StartPose.Wing) {
-          return Pathing.getHolonomicTargetPoseCommand(
-              Drivetrain.getPose().plus(new Transform2d(6, 0, new Rotation2d())));
+          return Pathing.getHolonomicTargetPoseCommand(new Pose2d(7.36, 1.62, new Rotation2d()));
         }
         return Pathing.getHolonomicFollowPathCommand(Pathing.loadPath(startPose.getName()));
 
