f01b59ef6274065dc3a122b47fd7e2e59d42375c
Sid T <thomas.siddharth@gmail.com>
2024-03-09 10:35:33 -0800

    Log autos

--- src/main/java/frc/robot/autos/AutoSelector.java
+++ src/main/java/frc/robot/autos/AutoSelector.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.auto.NamedCommands;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.logger.Logger;
 
 // TODO: Test autos
 /** Contains auto types, choosers, and compiler. */
@@ -41,6 +42,8 @@ public class AutoSelector {
     var startPose = chooser_startPose.getSelected();
     var starter = chooser_starter.getSelected();
     var body = chooser_body.getSelected();
+
+    Logger.log("AUTON: " + startPose.toString() + " " + starter.toString() + " " + body.toString());
 
     return startPose.getCommand().andThen(starter.getCommand()).andThen(body.getCommand(startPose));
   }
