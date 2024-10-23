c31c289319171e82b92f3777df2b29a1045f9bf3
Eric D <111144791+XrxcGitHub@users.noreply.github.com>
2024-10-22 17:55:38 -0700

    LimelightHelpers initial commit

--- src/main/java/frc/robot/devices/Limelight.java
+++ src/main/java/frc/robot/devices/Limelight.java
@@ -1,23 +1,40 @@
+/*
 package frc.robot.devices;
 
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.IntegerPublisher;
 import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-// TODO(dev): Add AprilTag functionality and parameterize subscribers to limit bandwith use
-/** Class for Limelight interfacing. */
+// Class for Limelight interfacing.
 public class Limelight {
-  /** Limelight datatable */
+
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+
+  // Read values periodically
+  double x = tx.getDouble(0.0);
+  double y = ty.getDouble(0.0);
+  double area = ta.getDouble(0.0);
+
+  // Post to smart dashboard periodically
+  SmartDashboard.putNumber("LimelightX", x);
+  SmartDashboard.putNumber("LimelightY", y);
+  SmartDashboard.putNumber("LimelightArea", area);
+
+  // Limelight datatable.
   private final NetworkTable m_limelight;
 
   private final DoubleSubscriber tv_sub, tx_sub, ty_sub, ta_sub, tid_sub, pipeline_sub;
   private final IntegerPublisher pipeline_pub, cam_pub;
 
-  /**
-   * Initialize a new limelight.
-   *
-   * @param limelight {@link NetworkTable} that points to the limelight data stream
-   */
+  // Initialize a new limelight.
+  // @param limelight {@link NetworkTable} that points to the limelight data stream.
+
   protected Limelight(NetworkTable limelight) {
     m_limelight = limelight;
 
@@ -32,52 +49,47 @@ public class Limelight {
     cam_pub = m_limelight.getIntegerTopic("camMode").publish();
   }
 
-  /** Returns horizontal angle to target (UNIT: degrees) */
+  // Returns horizontal angle to target in degrees.
   public double getTx() {
     return tx_sub.getAsDouble();
   }
 
-  /** Returns vertical angle to target (UNIT: degrees) */
+  // Returns vertical angle to target in degrees.
   public double getTy() {
     return ty_sub.getAsDouble();
   }
 
-  /** Returns percent area of target relative to camera */
+  // Returns percent area of target relative to camera.
   public double getArea() {
     return ta_sub.getAsDouble();
   }
 
-  /** Returns {@code true} if a valid target exists */
+  // Returns {@code true} if a valid target exists
   public boolean getValid() {
     return (tv_sub.getAsDouble() == 1) ? true : false;
   }
 
-  /** Returns ID of the targeted AprilTag */
+  // Returns ID of the targeted AprilTag.
   public int getTid() {
     return (int) tid_sub.getAsDouble();
   }
 
-  /** Returns active pipeline index [0..9] */
+  // Returns active pipeline index [0..9].
   public int getPipeline() {
     return (int) pipeline_sub.getAsDouble();
   }
 
-  /**
-   * Set the limelight pipeline. Recommended to wait for {@link Limelight#getPipeline()} to
-   * corroborate before using functional code that depends on it.
-   *
-   * @param pipeline pipeline index to set [0..9]
-   */
+  // Set the limelight pipeline. Recommended to wait for {@link Limelight#getPipeline()} to corroborate before using functional code that depends on it.
+  // @param pipeline pipeline index to set [0..9]
+
   public void setPipeline(int pipeline) {
     pipeline_pub.accept(pipeline);
   }
 
-  /**
-   * Set the limelight operation mode.
-   *
-   * @param isDriverCam {@code true} for driver camera, {@code false} for vision processing
-   */
+  // Set the limelight operation mode.
+  // @param isDriverCam {@code true} for driver camera, {@code false} for vision processing
   public void setDriverCam(boolean isDriverCam) {
     cam_pub.accept(isDriverCam ? 1 : 0);
   }
 }
+*/
